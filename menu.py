82e5ba47b08183564aa4611e33884a45bd80d4a4
Rayanna C. Santos <126517891+raychr@users.noreply.github.com>
2024-11-25 14:19:36 -0300

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,31 +1,29 @@
 import pygame
-import sys
 
 class Menu:
     def __init__(self, width, height, background, sound_file):
         self.width = width
         self.height = height
-        self.background= pygame.image.load(background)
+        self.background = pygame.image.load(background)
         self.sound = pygame.mixer.Sound(sound_file)
         self.running = True
 
     def show(self):
-        self.sound.play(-1)
+        self.sound.play(-1)  # Toca o som em loop
         while self.running:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                if event.type == pygame.KEYDOWN:
-                    self.running = False
-            screen.blit(self.background, (0, 0))
+                if event.type == pygame.QUIT:  # Usuário deseja fechar
+                    pygame.quit()
+                    exit()
+                if event.type == pygame.KEYDOWN:  # Qualquer tecla para iniciar o jogo
+                    self.running = False  # Sai do menu
+            screen.blit(self.background, (0, 0))  # Exibe a imagem de fundo
             pygame.display.flip()
-        self.sound.stop() 
-pygame.init()
+        self.sound.stop()  # Para o som quando o menu encerra
 
+
+# Configuração inicial da tela e Pygame
+pygame.init()
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Menu do Jogo")
-menu = Menu(WIDTH, HEIGHT, 'background.jpeg', 'thunder.wav')
-
-menu.show()
-pygame.quit()
