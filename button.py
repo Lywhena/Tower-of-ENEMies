f5fe9ec581b0085e19b547894de47133f6744d4b
Helena Veltri <helepveltri@gmail.com>
2024-12-02 16:27:56 -0300

    divisão de classes diferentes em arquivos diferentes

---
+++ button.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Button:
+    def __init__(self, text, x, y, width, height, font, color=WHITE, hover_color=(200, 200, 200)):
+        self.text = text
+        self.rect = pygame.Rect(x, y, width, height)
+        self.font = font
+        self.color = color
+        self.hover_color = hover_color
+
+    def draw(self, surface):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            pygame.draw.rect(surface, self.hover_color, self.rect)
+        else:
+            pygame.draw.rect(surface, self.color, self.rect)
+        pygame.draw.rect(surface, BLACK, self.rect, 2)  # Borda preta
+        text_surf = self.font.render(self.text, True, BLACK)
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        surface.blit(text_surf, text_rect)
+
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
---
+++ enemy.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Enemy:
+    def __init__(self, name, image_path, background_path, questions):
+        self.name = name
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (400, 400))
+        self.background = pygame.image.load(background_path)
+        self._questions = questions
+        self.current_question_index = 0
+        
+    def get_current_question(self):
+        return self._questions[self.current_question_index]
+
+    def next_question(self):
+        if self.current_question_index < len(self._questions) - 1:
+            self.current_question_index += 1
+            return True
+        return False
+
+    def reset_questions(self):
+        self.current_question_index = 0
+
+    def randomize_questions(self):
+        random.shuffle(self._questions)
+        for question in self._questions:
+            self._randomize_options(question)
+
+    def _randomize_options(self, question):
+        correct_answer = question['options'][question['answer']]
+        random.shuffle(question['options'])
+        question['answer'] = question['options'].index(correct_answer)
